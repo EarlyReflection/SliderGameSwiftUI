719e5a5cab51a4901da72a822467a754905c5b63
Vladimir Dvornikov <earlyreflections@protonmail.com>
2023-03-03 20:55:48 +0200

    Initial Commit

---
+++ SliderGameSwiftUI/SliderGameSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameSwiftUIApp.swift
+//  SliderGameSwiftUI
+//
+//  Created by Vladimir Dvornikov on 03/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
